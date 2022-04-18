c42179b59410b87829e7e85eb9a0328a3951dd4e
imgaram <s21049@gsm.hs.kr>
2022-04-18 20:28:18 +0900

    안드로이드 CleanArchitecture 예제 4-3: xml 추가, 수정

--- CleanArchitectureExample/data/src/main/java/com/example/data/githubexample/source/GithubRemoteSource.kt
+++ CleanArchitectureExample/data/src/main/java/com/example/data/githubexample/source/GithubRemoteSource.kt
@@ -1,8 +1,19 @@
 package com.example.data.githubexample.source
 
+import com.example.data.githubexample.api.GithubService
 import com.example.data.githubexample.model.GithubRepoRes
+import javax.inject.Inject
 
 // GithubRemoteSource 또한 인터페이스로 구현한 뒤 구현체를 별도로 갖는 방식으로 구현했다.
 interface GithubRemoteSource {
     suspend fun getRepos(owner: String): List<GithubRepoRes>
+}
+
+class GithubRemoteSourceImpl @Inject constructor(
+    private val githubService: GithubService
+) : GithubRemoteSource {
+
+    override suspend fun getRepos(owner: String): List<GithubRepoRes> {
+        return githubService.getRepos(owner)
+    }
 }
